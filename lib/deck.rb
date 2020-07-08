2714bff30335a0ce3c59002c0d1cefd7077c2272
QuantumWaver <sholl76@gmail.com>
2020-07-08 15:47:07 -0400

    some mucking around

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,10 +1,7 @@
 class Deck
   attr_reader :cards, :rank
 
-  cards = []
-
-
-  def initialize (cards)
+  def initialize(cards)
     @cards = cards
   end
 
@@ -15,9 +12,7 @@ class Deck
   def high_ranking_cards
     high_ranks = []
     @cards.each do |card|
-      if card.rank >= 11
-        high_ranks << card
-      end
+      high_ranks << card if card.rank >= 11
     end
     high_ranks
   end
@@ -30,6 +25,21 @@ class Deck
       end
     end
     ((100 / @cards.count.to_f) * high_ranks.count).round(2)
+  end
+
+  def jeff_high_ranking_cards
+    @cards.inject([]) do |accum, card|
+      accum << card if card.rank >= 11
+      accum
+    end
+  end
+
+  def jeff_percent_high_ranking
+    count_total_cards = @cards.length
+    return 0.0 unless count_total_cards.positive?
+
+    count_high_ranking_cards = jeff_high_ranking_cards.length
+    ((count_high_ranking_cards / count_total_cards.to_f) * 100).round(2)
   end
 
   def remove_card
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -5,38 +5,49 @@ require './lib/deck'
 require "pry"
 
 class DeckTest < Minitest::Test
-  #
-  # def test_it_exists
-  #   deck = Deck.new
-  #
-  #   assert_instance_of Deck, deck
-  # end
+  def test_it_exists
+    deck = Deck.new(nil)
 
-  # def test_it_has_attributes
-  #   card1 = Card.new(:diamond, 'Queen', 12)
-  #   card2 = Card.new(:spade, '3', 3)
-  #   card3 = Card.new(:heart, 'Ace', 14)
-  #
-  #   cards = [card1, card2, card3]
-  #
-  #   deck = Deck.new(cards)
-  #
-  #   assert_equal [card1, card2, card3], deck.cards
-  # end
+    assert_instance_of Deck, deck
+  end
 
-  # def test_cards_have_rank
-  #   card1 = Card.new(:diamond, 'Queen', 12)
-  #   card2 = Card.new(:spade, '3', 3)
-  #   card3 = Card.new(:heart, 'Ace', 14)
-  #
-  #   cards = [card1, card2, card3]
-  #
-  #   deck = Deck.new(cards)
-  #
-  #   assert_equal 12, deck.rank_of_card_at(0)
-  #   #binding.pry
-  # end
+  def test_it_has_attributes
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
 
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+
+    assert_equal [card1, card2, card3], deck.cards
+  end
+
+  def test_cards_have_rank
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:heart, 'Ace', 14)
+
+    cards = [card1, card2, card3]
+
+    deck = Deck.new(cards)
+
+    assert_equal 12, deck.rank_of_card_at(0)
+  end
+
+  def test_jeff_high_ranking_cards
+    card1 = Card.new(:diamond, 'Queen', 12)
+    card2 = Card.new(:spade, '3', 3)
+    card3 = Card.new(:spade, '2', 2)
+    card4 = Card.new(:heart, 'Ace', 14)
+    card5 = Card.new(:spade, 'King', 13)
+
+    cards = [card1, card2, card3, card4, card5]
+
+    deck = Deck.new(cards)
+
+    assert_equal [card1, card4, card5], deck.jeff_high_ranking_cards
+  end
 
   # def test_percentage_of_high_ranking_cards
   #   card1 = Card.new(:diamond, 'Queen', 12)
@@ -63,16 +74,16 @@ class DeckTest < Minitest::Test
     #   assert_equal card1, deck.remove_card
     # end
 
-    def test_card_can_be_added
-      card1 = Card.new(:diamond, 'Queen', 12)
-      card2 = Card.new(:spade, '3', 3)
-      card3 = Card.new(:heart, 'Ace', 14)
-      card4 = Card.new(:club, '5', 5)
+    # def test_card_can_be_added
+    #   card1 = Card.new(:diamond, 'Queen', 12)
+    #   card2 = Card.new(:spade, '3', 3)
+    #   card3 = Card.new(:heart, 'Ace', 14)
+    #   card4 = Card.new(:club, '5', 5)
 
-      cards = [card1, card2, card3]
+    #   cards = [card1, card2, card3]
 
-      deck = Deck.new(cards)
-      # binding.pry
-      assert_equal [card1, card2, card3, card4], deck.add_card(card4)
-    end
+    #   deck = Deck.new(cards)
+    #   # binding.pry
+    #   assert_equal [card1, card2, card3, card4], deck.add_card(card4)
+    # end
 end
